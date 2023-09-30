ef7a7d2ab83c9f5f90ab350e75c5b0a783b75f7a
Prince Sanguan <princesanguan44@gmail.com>
2023-09-30 14:36:33 +0800

    first push in my tutorial praktis lang para gumaling

---
+++ src/Mmain.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function Mmain() {
+  return <h1>Main Components</h1>;
+}
---
+++ src/Navbar.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Litrato from "./logo512.png";
+
+export default function Navbar() {
+  return (
+    <nav>
+      <img src={Litrato} alt="broken-image" />
+      <h1>reactFacts</h1>
+      <h2>React course Project !</h2>
+    </nav>
+  );
+}
